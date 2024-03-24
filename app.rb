1dc00d0e161ba12b88bd8886fef0a09ac046f695
benjamin <b.james.neustadt@gmail.com>
2024-03-24 20:11:22 +0000

    WIP: uploading a file to the POST route

--- app.rb
+++ app.rb
@@ -108,8 +108,10 @@ class Application < Sinatra::Base
       erb :admin
     end
 
-    post '/new_asset' do
-      new_video = params[:new_video]
+    post '/upload_new_asset' do
+      input_new_video = params[:new_video]
+      file_name_for_new_video = params[:new_video][:filename]
+      puts input_new_video
 
       # API Client Initialization
       assets_api = MuxRuby::AssetsApi.new
@@ -120,25 +122,33 @@ class Application < Sinatra::Base
       # ========== create-direct-upload ==========
       create_asset_request = MuxRuby::CreateAssetRequest.new
       create_asset_request.playback_policy = [MuxRuby::PlaybackPolicy::PUBLIC]
+
       create_upload_request = MuxRuby::CreateUploadRequest.new
+
       create_upload_request.new_asset_settings = create_asset_request
       create_upload_request.timeout = 3600
       create_upload_request.cors_origin = "philcluff.co.uk"
+
       upload = uploads_api.create_direct_upload(create_upload_request)
 
+      signed_upload_url = upload.data.url
+      endpoint = signed_upload_url + '/' + file_name_for_new_video
+
       # We will use:
-      #upload.data.url
 
       # ========== create-asset ==========
-      car = MuxRuby::CreateAssetRequest.new
-      car.input = [{:url => 'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4'}, {:url => "https://tears-of-steel-subtitles.s3.amazonaws.com/tears-fr.vtt", :type => "text", :text_type => "subtitles", :name => "French", :language_code => "fr", :closed_captions => false}]
-      create_response = assets_api.create_asset(car)
+      # car = MuxRuby::CreateAssetRequest.new
+      # car.input = [{:url => 'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4'}, {:url => "https://tears-of-steel-subtitles.s3.amazonaws.com/tears-fr.vtt", :type => "text", :text_type => "subtitles", :name => "French", :language_code => "fr", :closed_captions => false}]
+
+      create_asset_request.input = [{:url => endpoint}]
+      create_response = assets_api.create_asset(create_asset_request)
+
       assert create_response != nil
       assert create_response.data.id != nil
       puts "create-asset OK ✅"
     end
 
-  run! if app_file == $0
+  # run! if app_file == $0
 end
 
 #=+=+=+=+=+=+=+=+=+=+=+=+=+=+==+=+=+=+=+=+=+=+=+=+=+=+=+=+=++
