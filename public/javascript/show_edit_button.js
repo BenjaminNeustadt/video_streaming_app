91eb96810a025939c3796926ee4c27c360a9fbea
benjamin <b.james.neustadt@gmail.com>
2024-04-03 22:33:11 +0100

    WIP: Fix edit form shows with other asset actions

--- public/javascript/show_edit_button.js
+++ public/javascript/show_edit_button.js
@@ -1,15 +1,8 @@
 document.querySelectorAll('.edit-button').forEach(function(button) {
   button.addEventListener('click', function() {
-    // Get the corresponding asset actions container
-    var assetActions = this.parentElement.querySelector('.asset-actions');
-
-    // Toggle visibility of asset actions container and its children
+    var assetActions = this.closest('.video_asset').querySelector('.asset-actions');
     if (assetActions) {
-      var displayValue = (assetActions.style.display === 'none') ? 'block' : 'none';
-      assetActions.style.display = displayValue;
-      assetActions.querySelectorAll('*').forEach(function(element) {
-        element.style.display = displayValue;
-      });
+      assetActions.classList.toggle('show');
     }
   });
 });
