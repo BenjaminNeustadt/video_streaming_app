0fc8e87ae5c034307bbddb870ee0f719ee2ba25a
benjamin <b.james.neustadt@gmail.com>
2024-04-05 01:04:44 +0100

    Implement dark overlay when admin view entered on asset

--- public/javascript/show_asset_actions.js
+++ public/javascript/show_asset_actions.js
@@ -3,6 +3,7 @@ document.querySelectorAll('.edit-button').forEach(function(button) {
     var assetActions = this.closest('.video_asset').querySelector('.asset-actions');
     if (assetActions) {
       assetActions.classList.toggle('show');
+      darkenBackground();
     }
   });
 });
@@ -12,6 +13,32 @@ document.querySelectorAll('.close-admin-view').forEach(function(button) {
     var assetActions = this.closest('.video_asset').querySelector('.asset-actions');
     if (assetActions) {
       assetActions.classList.toggle('show');
+      removeDarkOverlay();
     }
   });
 });
+
+
+function darkenBackground() {
+  // Create and append a dark overlay element to the body
+  let overlay = document.querySelector('.video-overlay');
+
+  if (!overlay) {
+    overlay = document.createElement('div');
+    overlay.classList.add('video-overlay');
+    document.body.appendChild(overlay);
+  }
+
+  // Ensure that the overlay is visible
+  setTimeout(() => {
+    overlay.classList.add('overlay-active');
+  }, 0);
+}
+
+function removeDarkOverlay() {
+  const overlay = document.querySelector('.video-overlay');
+  if (overlay) {
+    // Remove the overlay element from the body
+    overlay.remove();
+  }
+}
