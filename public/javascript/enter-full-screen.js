e059229e5339bcf88caf40cf64ae8355e8d83a75
benjamin <b.james.neustadt@gmail.com>
2024-04-07 17:14:56 +0100

    WIP: overlay functions when full screen

--- public/javascript/enter-full-screen.js
+++ public/javascript/enter-full-screen.js
@@ -1,30 +1,27 @@
 // Add event listeners to each full-screen button
+
+
 document.querySelectorAll('.fullscreen-button').forEach(button => {
   button.addEventListener('click', () => {
     const muxplayer = button.closest('.video_asset').querySelector('.video-stream mux-player');
+    const overlay = button.closest('.video_asset').querySelector('.full-screen-overlay');
     toggleFullScreen(muxplayer);
-    removeHover();
+    showOverlay(overlay);
   });
 });
+
+function showOverlay(element) {
+    element.style.display = 'block'; // Show overlay
+    element.style.opacity = '1';
+    console.log('overlay called');
+};
 
 // Function to toggle full-screen mode
 function toggleFullScreen(element) {
   console.log('toggleFullScreen function called');
   if (!element.classList.contains('video-fullscreen')) {
     element.classList.add('video-fullscreen');
-    // document.body.classList.add('fullscreen-mode'); // Add class to body
   } else {
-    // element.classList.remove('video-fullscreen');
-    // document.body.classList.remove('fullscreen-mode'); // Remove class from body
+    element.classList.remove('video-fullscreen');
   }
 }
-
-  function removeHover() {
-    console.log('Hover functionality removed');
-    const elements = document.querySelectorAll('*');
-    elements.forEach(element => {
-      // Remove any existing hover-related styles
-      element.style.removeProperty('hover');
-      element.style.removeProperty('pointer-events');
-    });
-  }
