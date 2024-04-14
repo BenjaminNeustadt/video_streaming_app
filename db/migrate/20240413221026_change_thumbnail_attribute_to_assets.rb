d6c09e8c0d8492923ae59dfc3086bcd30fb7433d
benjamin <b.james.neustadt@gmail.com>
2024-04-14 20:40:10 +0100

    Remove migration that gets in the way
    * because of the new attributes that we are usinb on the schema for an
    asset object... we remove previous migrations that get inthe way of the
    migration command when starting up the database from afresh.

--- db/migrate/20240413221026_change_thumbnail_attribute_to_assets.rb
+++
@@ -1,7 +0,0 @@
-class ChangeThumbnailAttributeToAssets < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :assets, :thumbnail_position
-
-    add_column :assets, :thumbnail_time, :string
-  end
-end
