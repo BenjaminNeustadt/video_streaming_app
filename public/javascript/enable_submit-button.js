22e7cd4268f1ce9e99de096749afcde2ba192236
benjamin <b.james.neustadt@gmail.com>
2024-04-26 12:40:31 +0100

    FIX: submit button enabled on upload success
    * Modify the javascript path for status-message, having changed the html
    structure

--- public/javascript/enable_submit-button.js
+++ public/javascript/enable_submit-button.js
@@ -1,6 +1,11 @@
+// This function runs every second when on the admin page,
+// when it discovers the 'Upload complete!' MessageChannel, it enables click
+// functionality on the submit button.
+
 function enableSubmitButton() {
   console.log("Checking upload status...");
-  const statusMessage = document.querySelector("body > main > div.upload_asset_metadata > div > mux-uploader").shadowRoot.querySelector("mux-uploader-status").shadowRoot.querySelector("#status-message");
+  const statusMessage = document.querySelector("body > main > div.upload_asset_metadata > div > form > div.upload_asset > mux-uploader").shadowRoot.querySelector("mux-uploader-status").shadowRoot.querySelector("#status-message");
+  // console.log("Status Message Element:", statusMessage);
 
   if (statusMessage && statusMessage.innerText.trim() === 'Upload complete!') {
     console.log("Upload complete message found.");
@@ -18,7 +23,3 @@ function enableSubmitButton() {
 }
 
 const uploadStatusCheckInterval = setInterval(enableSubmitButton, 1000);
-
-// This function runs every second when on the admin page,
-// when it discovers the 'Upload complete!' MessageChannel, it enables click
-// functionality on the submit button.
