f139ea01bc0f808120a48b63a9180b26dff7ddf7
benjamin <b.james.neustadt@gmail.com>
2024-04-27 21:44:34 +0100

    Styles: branding title in dark mode and navigation styles

--- public/javascript/darkmode-toggle.js
+++ public/javascript/darkmode-toggle.js
@@ -2,6 +2,7 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
 darkModeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     document.querySelector('nav').classList.toggle('dark-mode-nav'); // Add or remove 'dark-mode-nav' class to <nav>
+    document.querySelector('h1').classList.toggle('dark-mode-h1'); // Add or remove 'dark-mode-nav' class to <nav>
 
     const darkModeEnabled = document.body.classList.contains('dark-mode');
     document.cookie = `darkModeEnabled=${darkModeEnabled}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
