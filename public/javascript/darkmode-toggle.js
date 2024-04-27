cc98431731f6d9bc1a01be2cdddea8294d868c86
benjamin <b.james.neustadt@gmail.com>
2024-04-27 20:00:14 +0100

    WIP: styles on navigation bar when dark mode

--- public/javascript/darkmode-toggle.js
+++ public/javascript/darkmode-toggle.js
@@ -1,6 +1,7 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 darkModeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    document.querySelector('nav').classList.toggle('dark-mode-nav'); // Add or remove 'dark-mode-nav' class to <nav>
 
     const darkModeEnabled = document.body.classList.contains('dark-mode');
     document.cookie = `darkModeEnabled=${darkModeEnabled}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
