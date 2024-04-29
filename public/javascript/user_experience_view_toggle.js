a78620673e87304e0af32e01b2abd89b6b61aef5
benjamin <b.james.neustadt@gmail.com>
2024-04-30 00:55:54 +0100

    refactor: user metrics view buttons script

---
+++ public/javascript/user_experience_view_toggle.js
@@ -0,0 +1,20 @@
+  document.addEventListener("DOMContentLoaded", function() {
+    const showAllButton = document.getElementById("show-all");
+    const showLessButton = document.getElementById("show-less");
+
+    const firstTenUsers = document.getElementById("first-10-user-metrics");
+    const allUsers = document.getElementById("all-user-metrics");
+
+    showAllButton.addEventListener("click", function() {
+      console.log("Show all button clicked");
+        firstTenUsers.hidden = true;
+        allUsers.hidden = false;
+      });
+
+    showLessButton.addEventListener("click", function() {
+      console.log("Show less button clicked");
+        allUsers.hidden = true;
+        firstTenUsers.hidden = false;
+    });
+  });
+
