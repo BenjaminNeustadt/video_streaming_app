ba1a6c041962b2c95b84b51e035fdd0fb5fe6e68
benjamin <b.james.neustadt@gmail.com>
2024-05-01 22:49:30 +0100

    styles: change dark mode button text content

--- public/javascript/darkmode-toggle.js
+++ public/javascript/darkmode-toggle.js
@@ -1,9 +1,12 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 darkModeToggle.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+    const darkModeEnabled = document.body.classList.toggle('dark-mode');
+    // document.body.classList.toggle('dark-mode');
     document.querySelector('nav').classList.toggle('dark-mode-nav'); // Add or remove 'dark-mode-nav' class to <nav>
     document.querySelector('h1').classList.toggle('dark-mode-h1'); // Add or remove 'dark-mode-nav' class to <nav>
 
-    const darkModeEnabled = document.body.classList.contains('dark-mode');
+    darkModeToggle.textContent = darkModeEnabled ? 'Light Mode' : 'Dark Mode';
+
+    // const darkModeEnabled = document.body.classList.contains('dark-mode');
     document.cookie = `darkModeEnabled=${darkModeEnabled}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
 });
