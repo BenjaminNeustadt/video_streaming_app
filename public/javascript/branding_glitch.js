212634fc79a92662934c84f49eec5ce3ad09abfc
benjamin <b.james.neustadt@gmail.com>
2024-05-05 18:41:16 +0100

    change reroute after glicth

--- public/javascript/branding_glitch.js
+++ public/javascript/branding_glitch.js
@@ -1,29 +1,64 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 let interval = null;
 
-document.querySelector("h1").onmouseover = event => {  
+// Function to perform glitch effect
+function performGlitchEffect(element, callback) {
   let iteration = 0;
   
   clearInterval(interval);
   
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    element.innerText = element.innerText
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return element.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
+    if(iteration >= element.dataset.value.length){ 
       clearInterval(interval);
+      callback();
     }
     
     iteration += 1 / 3;
   }, 30);
 }
+
+// Event listener for mouseover on the h1 element
+document.querySelector("h1").onmouseover = event => {  
+  // Perform glitch effect
+  performGlitchEffect(event.target, () => {
+    // Redirect to /selection after glitch animation completes
+    window.location.href = "/selection";
+  });
+}
+
+// document.querySelector("h1").onmouseover = event => {  
+//   let iteration = 0;
+  
+//   clearInterval(interval);
+  
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
+      
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
+    
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
+    
+//     iteration += 1 / 3;
+//   }, 30);
+// }
