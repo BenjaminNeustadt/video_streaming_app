fbcfbed4213d8abea5280326bdd7dece04158162
benjamin <b.james.neustadt@gmail.com>
2024-06-01 17:19:43 +0100

    feat: dark mode taken from local storage

--- public/javascript/darkmode-toggle.js
+++ public/javascript/darkmode-toggle.js
@@ -21,24 +21,64 @@ function updateButtonContent() {
 // Initial update when the page loads
 updateButtonContent();
 
-function logCookie() {
-    console.log(document.cookie);
+function logLocalStorage() {
+    console.log(localStorage.getItem("dark-mode"));
 }
+
+// Function to enable dark mode
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    document.querySelector('nav').classList.add('dark-mode-nav');
+    document.querySelector('h1').classList.add('dark-mode-h1');
+    localStorage.setItem("dark-mode", "enabled");
+};
+
+// Function to disable dark mode
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode');
+    document.querySelector('nav').classList.remove('dark-mode-nav');
+    document.querySelector('h1').classList.remove('dark-mode-h1');
+    localStorage.setItem("dark-mode", "disabled");
+};
+
+// Check the initial dark mode status from localStorage
+let darkMode = localStorage.getItem("dark-mode");
+if (darkMode === "enabled") {
+    enableDarkMode(); // set state of darkMode on page load
+} else {
+    disableDarkMode(); // ensure dark mode is disabled if not enabled
+}
+
+// Update button content based on initial dark mode status
+updateButtonContent();
 
 // Event listener for toggling dark mode
 darkModeToggle.addEventListener('click', function() {
-    const darkModeEnabled = document.body.classList.toggle('dark-mode');
-    document.querySelector('nav').classList.toggle('dark-mode-nav');
-    document.querySelector('h1').classList.toggle('dark-mode-h1');
+    darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === "disabled" || darkMode === null) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 
     // Update button content after toggling dark mode
     updateButtonContent();
+    logLocalStorage();
+});
+// Event listener for toggling dark mode
+// darkModeToggle.addEventListener('click', function() {
+//     const darkModeEnabled = document.body.classList.toggle('dark-mode');
+//     document.querySelector('nav').classList.toggle('dark-mode-nav');
+//     document.querySelector('h1').classList.toggle('dark-mode-h1');
+
+    // Update button content after toggling dark mode
+    // updateButtonContent();
 
     // Store dark mode status in a cookie
     // document.cookie = `darkModeEnabled=${darkModeEnabled}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-    document.cookie = `darkModeEnabled=${darkModeEnabled}; SameSite=Strict`;
-    logCookie();
-});
+    // document.cookie = `darkModeEnabled=${darkModeEnabled}; SameSite=Strict`;
+    // logCookie();
+// });
 
 // Event listener for window resize to update button content
 window.addEventListener('resize', updateButtonContent);
